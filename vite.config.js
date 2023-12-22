b0142ed013d7a54755169617dcbc6f678488cd6a
Kovtunets Mykola <kovtunets73@gmail.com>
2023-12-22 19:44:52 +0100

    no command parameter in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     base: "./", // each path to a file connected to index.html starts with this
     root: "src", // where index.html is located
