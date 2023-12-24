0adabcf13e087a44c4a4951358193bbce810c624
Kovtunets Mykola <kovtunets73@gmail.com>
2023-12-24 10:56:31 +0100

    partials are added and multiple htmls

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from "vite";
+import glob from "glob";
+import injectHTML from "vite-plugin-html-inject";
+import FullReload from "vite-plugin-full-reload";
 
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   return {
-    base: "./", // each path to a file connected to index.html starts with this
-    root: "src", // where index.html is located
-    build: {
-      outDir: "../dist", // distribution folder
+    define: {
+      [command === "serve" ? "global" : "_global"]: {},
     },
+    root: "src", // the root directory (where index.html is located)
+    base: "./", // the base of the paths in output directory (what paths in the dist directory are gonna begin with)
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync("./src/**.html"),
+        // input: glob.sync("./src/**/**.html"), if you wanna link all htmls in all folders in the src folder
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor";
+            }
+          },
+          entryFileNames: "commonHelpers.js",
+        },
+      },
+      outDir: "../dist", // the output directory (dist folder)
+    },
+    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
   };
 });
