4006cd9ea0083f9768106989c20156bdd099bd73
Kovtunets Mykola <kovtunets73@gmail.com>
2023-12-24 16:57:56 +0100

    vite.config is improved and simplified, dependencies are updated

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,17 @@
 import { defineConfig } from "vite";
-import glob from "glob";
+import { globSync } from "glob";
 import injectHTML from "vite-plugin-html-inject";
-import FullReload from "vite-plugin-full-reload";
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
     root: "src", // the root directory (where index.html is located)
     base: "./", // the base of the paths in output directory (what paths in the dist directory are gonna begin with)
     build: {
-      sourcemap: true,
-
       rollupOptions: {
-        input: glob.sync("./src/**.html"),
-        // input: glob.sync("./src/**/**.html"), if you wanna link all htmls in all folders in the src folder
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return "vendor";
-            }
-          },
-          entryFileNames: "commonHelpers.js",
-        },
+        input: globSync("./src/**.html"),
       },
       outDir: "../dist", // the output directory (dist folder)
     },
-    plugins: [injectHTML(), FullReload(["./src/**/**.html"])],
+    plugins: [injectHTML()],
   };
 });
