b347d67c1ebec8c6c74e7150792680537821c06e
Kovtunets Mykola <kovtunets73@gmail.com>
2023-12-27 23:00:49 +0100

    global object works

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import { globSync } from "glob";
 import injectHTML from "vite-plugin-html-inject";
 
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {}, // sets the global object
+    },
     root: "src", // the root directory (where index.html is located)
     base: "./", // the base of the paths in output directory (what paths in the dist directory are gonna begin with)
     build: {
